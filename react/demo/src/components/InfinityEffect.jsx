b7b3341b4f6d5be8d03382b491783cf572d8ba12
haivoDA22TTD <vochihai7@gmail.com>
2025-04-24 16:38:39 +0700

    add file Infinity.JSX Infinity.CSS Main.JSX

---
+++ react/demo/src/components/InfinityEffect.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './InfinityEffect.css';
+
+function InfinityEffect() {
+  return (
+    <div className="infinity-container">
+      <div className="infinity">∞</div>
+    </div>
+  );
+}
+
+export default InfinityEffect;
